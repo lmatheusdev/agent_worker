75a9f194b1c2061f609a0dc9d267c378f675b732
lmatheusdev <leandrothferraz@gmail.com>
2026-01-08 23:51:40 -0300

    add port to render

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.chat import router as chat_router
 from services.llm import init_rag
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -19,3 +21,7 @@ app.add_middleware(
 )
 
 app.include_router(chat_router, prefix="/api")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
